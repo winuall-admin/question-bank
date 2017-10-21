36bd87552b276bc3f14e30109a24dd606431f8b8
Fernando Valencia <fvalenciaf@gmail.com>
2017-10-21 02:55:23 -0300

    Add user list and show user view.

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('navbar-header')
+<ul class="nav navbar-nav">
+  <li><a href="{{ url('/admin/') }}">User list</a></li>
+  </li>
+</ul>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,3 +18,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('admin')->middleware('isAdmin')->group(function () {
+    Route::get('/', 'AdminController@index')->name('dashboard');
+    Route::get('/users/{user}', 'AdminController@getUsers')->name('show_user');
+});
