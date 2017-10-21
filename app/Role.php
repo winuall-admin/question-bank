7ddbf088ec93d46ab58638e313d4806e6e5d3d71
Stefan Brankovik <10259827+KenoKokoro@users.noreply.github.com>
2017-10-21 17:34:33 +0200

    Add seeder for roles (#18)

--- app/Role.php
+++ app/Role.php
@@ -6,11 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
+    const OPERATOR = 'Operator';
+    const QUESTION_REVIEWER = 'Question Reviewer';
+    const ADMIN = 'Admin';
+
     protected $fillable = [
         'role'
     ];
 
-    public function user(){
+    public function user() {
         return $this->hasMany('App\User');
     }
 }
