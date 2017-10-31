9084b42e432c2b8f05600f6172bf22d0bcfc7177
SHUBHANG ARORA <shubhang.arora252@gmail.com>
2017-11-01 01:24:42 +0530

    Store method for Question

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Question;
 class QuestionController extends Controller
 {
-
+      public function store(Request $request)
+      {
+          Question::create([
+            'question'  =>  $request['question'],
+            'topic_id'  =>  $request['topic_id'],
+            'user_id'   =>  Auth::user()->id
+          ]);
+      }
 }
